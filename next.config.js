5a18364564dfae20d4c2189cb83f6eb380c6aa26
ronald-it <ronald.cirgofer@hotmail.com>
2024-06-03 20:11:18 +0200

    Aanpassingen gemaakt om console logs in browser mogelijk te maken. Werkt nog niet.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-    output: "export",
-    reactStrictMode: true,
-  };
-  
-  module.exports = nextConfig;
+  reactStrictMode: true,
+  output: "export",
+  env: {
+    NEXT_PUBLIC_BASE_PATH: process.env.NODE_ENV === 'production' ? '/social-proof-section' : '',
+  },
+};
+
+module.exports = nextConfig;
