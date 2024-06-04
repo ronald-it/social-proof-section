95b16f1aa6d98131688546ebd5cdfdc1dc5e54f6
ronald-it <ronald.cirgofer@hotmail.com>
2024-06-04 14:27:20 +0200

    Teruggezet naar eerdere instellingen. Nu afbeeldingen werkende maken voor Github pages.

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,11 @@
-"use client";
-import { useEffect } from "react";
 import CustomImage from "./components/CustomImage";
 
 export default function Home() {
-  useEffect(() => {
-    console.log("this should only be logged in the browser console");
-  }, []);
   return (
     <main>
-      <span style={{ border: "solid 1px white", backgroundColor: "red" }}>
-        <CustomImage src="/vercel.svg" alt="Next.js logo" width={180} height={37} priority />
+      <span style={{border: "solid 1px white", backgroundColor: "red"}}>
+      <CustomImage src="/next.svg" alt="Next.js logo" width={180} height={37} priority />
       </span>
-      <p>Test</p>
-    </main>
+      <p>Test</p>    </main>
   );
 }
