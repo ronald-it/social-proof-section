3d23d3dcfd397cb2ca4c29ecf2ff093f77f178f3
ronald-it <ronald.cirgofer@hotmail.com>
2024-06-05 23:36:57 +0200

    Testimonial component af

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,9 @@ export default function Home() {
   return (
   <main>
     <section>
-      <Testimonial imageSrc="/images/image-colton.jpg" imageAlt="Picture of Colton" />
-      <Testimonial imageSrc="/images/image-irene.jpg" imageAlt="Picture of Irene" />
-      <Testimonial imageSrc="/images/image-anne.jpg" imageAlt="Picture of Anne" />
+      <Testimonial imageSrc="/images/image-colton.jpg" imageAlt="Picture of Colton" name="Colton Smith" quote="We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!" />
+      <Testimonial imageSrc="/images/image-irene.jpg" imageAlt="Picture of Irene" name="Irene Roberts" quote="Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery." />
+      <Testimonial imageSrc="/images/image-anne.jpg" imageAlt="Picture of Anne" name="Anne Wallace" quote="Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone" />
     </section>
       </main>
   );
--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,8 +1,15 @@
 import CustomImage from "../CustomImage/CustomImage";
 import styles from './Testimonial.module.scss';
 
-export default function Testimonial({imageSrc, imageAlt}) {
-  return <article className={styles.article}>
-  <CustomImage src={imageSrc} alt={imageAlt} width={100} height={100} priority />
+export default function Testimonial({imageSrc, imageAlt, name, quote}) {
+  return <article className={styles.testimonial}>
+    <div className={styles.testimonial__header}>
+    <CustomImage className={styles.testimonial__image} src={imageSrc} alt={imageAlt} width={100} height={100} priority />
+    <div className={styles.testimonial__info}>
+    <span className={styles.testimonial__name}>{name}</span>
+    <span className={styles.testimonial__status}>Verified Buyer</span>
+    </div>
+    </div>
+  <p className={styles.testimonial__quote}>&quot;{quote}&quot;</p>
   </article>
 }
